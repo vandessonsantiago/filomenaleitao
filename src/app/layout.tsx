31f5463772b5a639a1f4b4da309c85df2a3a4e5e
vandessonsantiago <vandesson.santiago@outlook.com>
2024-07-10 11:40:58 +0100

    opengraph-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,17 +9,6 @@ export const metadata: Metadata = {
   title: "Gabinete de Pisicologia - Filomena Leitão",
   description:
     "Gabinete de Psicologia Filomena Leitão, um espaço dedicado a você! Aqui, você se sentirá seguro e apoiado. Explore a oportunidade de (re)construir sua vida, aprendendo a dar significado a cada momento e enfrentando seus desafios com apoio especializado.",
-
-  openGraph: {
-    title: "Gabinete de Pisicologia - Filomena Leitão",
-    description:
-      "Gabinete de Psicologia Filomena Leitão, um espaço dedicado a você! Aqui, você se sentirá seguro e apoiado.",
-    images: [
-      {
-        url: "./opengraph-image.png",
-      },
-    ],
-  },
 };
 
 export default function RootLayout({
